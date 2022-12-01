ee22dd8f876457ba83070ea92224b321f557db21
immu0001 <imran.malik01@icloud.com>
2022-12-02 01:19:16 +0530

    TDD itr-1 added subtract function, test

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,7 @@ Calculator Functions
 def add(x, y):
     """ Add x and y and return result. """
     return x + y
+
+def subtract(x, y):
+    """ Subtract x from y and return result. """
+    return y - x
