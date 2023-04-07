92d349d30d8e5562cfd80d876064f0ef226f9795
immu0001 <imran.malik01@icloud.com>
2023-04-07 14:27:12 +0530

    Added Create New User functionality into django admin site page.

--- app/core/admin.py
+++ app/core/admin.py
@@ -20,11 +20,26 @@ class UserAdmin(BaseUserAdmin):
         'fields' : (
                 'is_active',
                 'is_staff',
-                'is-superuser',
+                'is_superuser',
                     )
             }
         ),
-        (_('Important dates'), {'fields': ('last_login',)}),
+        (_('Important Dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                    'email',
+                    'password1',
+                    'password2',
+                    'name',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+        }),
     )
 
 admin.site.register(models.User, UserAdmin)
