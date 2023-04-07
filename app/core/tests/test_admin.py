cd1b508acf92428a1da2d46a33cd88d855983f48
immu0001 <imran.malik01@icloud.com>
2023-04-07 13:23:05 +0530

    implimentation for custom admin user model added.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -10,11 +10,24 @@ from django.test import Client
 class AdminSiteTests(TestCase):
     """ Tests for Django admin."""
 
-    def setUp(self) -> None:
+    def setUp(self):
         """ Create User and Client."""
         self.client = Client()
-        self.admin_user = get_user_model().objects.create_super_user(
+        self.admin_user = get_user_model().objects.create_superuser(
             email = 'admin@example.com',
             password = 'testpass123',
         )
         self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'user@example.com',
+            password = 'testpass123',
+            name = 'Test User'
+        )
+
+    def test_user_list(self):
+        """ Test that users are listed on page."""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
