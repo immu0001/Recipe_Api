aa77000dd9a8b3ca58a1fc47f54842e2af532450
immu0001 <imran.malik01@icloud.com>
2023-04-23 01:43:35 +0530

    tag delete test, view using mixins-- added.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -39,7 +39,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.DestroyModelMixin,
+                mixins.UpdateModelMixin,
+                mixins.ListModelMixin,
+                viewsets.GenericViewSet):
     """Manage Tags in the Database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
